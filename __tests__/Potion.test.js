c6f23d6c9159733fa075e7a5d63a488bf27ddf1b
Irina Skitayeva <irina.skitayeva@gmail.com>
2021-01-05 23:28:37 -0500

    Finished the Game code writing

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,12 +1,13 @@
-const Potion = require("../lib/Potion.js");
-// test("creates a health potion object", () => {
-//   const potion = new Potion("health");
+const Potion = require('../lib/Potion.js');
 
-//   expect(potion.name).toBe("health");
-//   expect(potion.value).toEqual(expect.any(Number));
-// });
+test('creates a health potion object', () => {
+  const potion = new Potion('health');
 
-test("creates a random potion object", () => {
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('creates a random potion object', () => {
   const potion = new Potion();
 
   expect(potion.name).toEqual(expect.any(String));
